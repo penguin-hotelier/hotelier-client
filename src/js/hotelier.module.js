9663f23fe93fa53c0b87b8c915239241691b2e02
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-04-04 15:55:08 -0400

    Added state for html login template

--- src/js/hotelier.module.js
+++ src/js/hotelier.module.js
@@ -1,6 +1,20 @@
 (function() {
   'use strict';
 
-  angular.module('hotelier', []);
+  angular.module('hotelier', ['ui.router'])
+    .config(routerConfig);
+
+  routerConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+  function routerConfig($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state({
+        name: 'login',
+        url: '',
+        templateUrl: 'views/login.template.html',
+        controller: 'LoginController',
+        controllerAs: 'loginCtrl'
+      });
+  }
 
 }());

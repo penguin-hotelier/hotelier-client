dbdd56f8d5eb62c2f533748343df3c31cd5e5566
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-04-04 15:12:02 -0400

    Initital set up for login service and controller

---
+++ src/js/login.controller.js
@@ -0,0 +1,17 @@
+(function() {
+  'use strict';
+
+  angular.module('hotelier')
+    .controller('LoginController', LoginController);
+
+  /**
+   * Handles the staff login
+   * @return {void}
+   */
+  function LoginController() {
+    
+  }
+
+
+
+}());
---
+++ src/js/login.service.js
@@ -0,0 +1,21 @@
+(function() {
+  'use strict';
+
+  angular.module('hotelier')
+    .factory('LoginService', LoginService);
+
+  LoginService.$inject = ['$http'];
+
+  /**
+   * Creates a new LoginService
+   * @param {Function}  $http The service for making ajax calls
+   * @return {Object}         The service's API methods
+   */
+  function LoginService($http) {
+
+  }
+
+
+
+
+}());

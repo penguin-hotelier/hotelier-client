7b5149711265e43d5077d8ab4e7696f1535b65fe
Nadia Mughal <nmughal14@gmail.com>
2017-04-05 18:51:00 -0400

    Injected the GuestService into the GuestController, added a docblock, and began setting up the createGuest function

--- src/js/guest.controller.js
+++ src/js/guest.controller.js
@@ -1,13 +1,31 @@
-(function() {
+  (function() {
   'use strict';
 
   angular.module('hotelier')
     .controller('GuestController', GuestController);
 
+  GuestController.$inject = ['GuestService'];
+
+  /**
+   * Creates GuestController's constructor
+   *
+   */
   function GuestController() {
     let vm = this;
 
+    vm.newGuest = {};
+
+    /**
+     * Adds a new guest and uses GuestService
+     * to communicate the data
+     * @return
+     */
     vm.createGuest = function createGuest() {
+
+      GuestService.createGuest() {
+        
+      }
+
 
     };
   }

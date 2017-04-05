1cbf6204b33fdcc06adb51191410f91943ab3ad0
David Steed <davidasteed@gmail.com>
2017-04-05 14:54:02 -0400

    Enabled UI-routing for make reservation template page

--- src/js/hotelier.module.js
+++ src/js/hotelier.module.js
@@ -1,6 +1,27 @@
 (function() {
   'use strict';
 
-  angular.module('hotelier', []);
+  angular.module('hotelier', ['ui.router'])
+    .config(routerConfig);
+
+  routingConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+  function routerConfig($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state({
+        name: 'login',
+        url: '/login',
+        templateUrl: 'views/login.template.html',
+        controller: 'LoginController',
+        controllerAs: 'loginCtrl'
+      })
+      .state({
+        name: 'make-reservation',
+        url: '/make-reservation',
+        templateUrl: 'views/make-reservation.template.html',
+        controller: 'ReservationController',
+        controllerAs: 'resCtrl'
+      });
+  }
 
 }());

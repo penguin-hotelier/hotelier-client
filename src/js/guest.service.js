a725fb7ca91d953f088907c1b8e55b64b7585467
Nadia Mughal <nmughal14@gmail.com>
2017-04-05 21:29:04 -0400

    CreateGuest function executes but gets back a 422 error message

--- src/js/guest.service.js
+++ src/js/guest.service.js
@@ -1,28 +1,32 @@
 (function() {
   'use strict';
 
-  anguler.module('hotelier')
+  angular.module('hotelier')
     .factory('GuestService', GuestService);
 
-  GuestService.$inject = ['$http'];
+  GuestService.$inject = ['$http', 'UserService'];
 
-  function GuestService($http) {
+  function GuestService($http, UserService) {
+    console.log('Creating a Guest Service');
 
-    function createGuest(guest) {
+    function createGuest(newGuest) {
+      console.log('This is the new Guest', newGuest);
       return $http({
         url: 'https://penguin-hotelier-api.herokuapp.com/api/Guests',
         method: 'post',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
+          'Authorization': UserService.getToken()
         },
         data: {
-          'fullName': '',
-          'phoneNumber': '',
-          'email': ''
+          'fullName': newGuest.fullName,
+          'phone': newGuest.phone,
+          'email': newGuest.email
         }
       })
-      .then(function handleResponse(response) {
-        return response.data;
+      .then(function handleResponse(responseObj) {
+        console.log(responseObj.data);
+        return responseObj.data;
       });
     }
 

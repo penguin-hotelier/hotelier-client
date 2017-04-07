2849461d378d82327e2d8406ed88e64d969d58e3
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-04-07 15:55:18 -0400

    Changed the spacing to match the standard for the project

--- src/test/specs/user.service.spec.js
+++ src/test/specs/user.service.spec.js
@@ -13,20 +13,20 @@
         }));
 
         describe('login', function() {
-          it('should fail if a string is not provided', function() {
-              let returnValue = UserService.login({email: 'jordan@hotelier.com', password: 'foobar'});
-              expect(returnValue.then).to.be.a('function');
-              expect(returnValue.catch).to.be.a('function');
+            it('should fail if a string is not provided', function() {
+                let returnValue = UserService.login({email: 'jordan@hotelier.com', password: 'foobar'});
+                expect(returnValue.then).to.be.a('function');
+                expect(returnValue.catch).to.be.a('function');
 
-              returnValue
-                  .then(function() {
-                      done('We should not resolve with a bad argument');
-                  })
-                  .catch(function handleError(err) {
-                      done();
-                  });
-          });
+                returnValue
+                .then(function() {
+                    done('We should not resolve with a bad argument');
+                })
+                .catch(function handleError(err) {
+                    done();
+                });
+            });
         });
-
+        
     });
 }());

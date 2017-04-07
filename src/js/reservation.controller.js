69158895557dccfc4b0682fe61fbd63ac719e40b
David Steed <davidasteed@gmail.com>
2017-04-07 16:07:52 -0400

    make js files in this branch compliant with team indenting standard and removed diagnostic logging"

--- src/js/reservation.controller.js
+++ src/js/reservation.controller.js
@@ -1,52 +1,42 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular.module('hotelier')
-    .controller('ReservationController', ReservationController);
+    angular.module('hotelier')
+        .controller('ReservationController', ReservationController);
 
-  ReservationController.$inject = ['ReservationService'];
-
-  /**
-   * [ReservationController constructor]
-   * @param {Object} ReservationService [inject dependenc(ies)]
-   */
-  function ReservationController(ReservationService) {
-    let vm = this;
-    vm.newReservation = {};
-    vm.reservations = ReservationService.getReservations();
-
-    // error message and state
-    // for Controller to send to the View as necessary
-    vm.errorMessage = null;
-    vm.hasError = false;
+    ReservationController.$inject = ['ReservationService'];
 
     /**
-     * [makeReservation description]
-     * @param  {Object} newRes [JS Object containing new reservation info]
-     * @return {void}
+     * [ReservationController constructor]
+     * @param {Object} ReservationService [inject dependenc(ies)]
      */
-    vm.makeReservation = function makeReservation(newRes) {
-      // basic validation of form Object
-      if (!newRes || newRes.length === 0 ||
-        Array.isArray(newRes) || typeof(newRes) !== 'object') {
-          vm.hasError = true;
-          vm.errorMessage = 'Sorry, invalid reservation information provided';
-          return;
-        }
+    function ReservationController(ReservationService) {
+        let vm = this;
+        vm.newReservation = {};
+        vm.reservations = ReservationService.getReservations();
 
-      // basic data validation
-      // Sun Jan 01 2017 00:00:00 GMT-0500 (EST)
+        // error message and state
+        // for Controller to send to the View as necessary
+        vm.errorMessage = null;
+        vm.hasError = false;
 
-      console.log('checkinDate for 1/1/2017 is ', newRes.checkinDate);
-      // if (newRes.checkinDate)
-      let testDate = new Date(newRes.checkinDate);
-      console.log('testDate is ', testDate);
+        /**
+         * [makeReservation description]
+         * @param  {Object} newRes [JS Object containing new reservation info]
+         * @return {void}
+         */
+        vm.makeReservation = function makeReservation(newRes) {
+          // basic validation of form Object
+          if (!newRes || newRes.length === 0 ||
+            Array.isArray(newRes) || typeof(newRes) !== 'object') {
+              vm.hasError = true;
+              vm.errorMessage = 'Sorry, invalid reservation information provided';
+              return;
+            }
 
+        ReservationService.makeReservation(newRes);
+      };
 
-
-      ReservationService.makeReservation(newRes);
-    };
-
-  }
+    }
 
 }());
--- src/js/reservation.service.js
+++ src/js/reservation.service.js
@@ -1,56 +1,52 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular.module('hotelier')
-  .factory('ReservationService', ReservationService);
+    angular.module('hotelier')
+        .factory('ReservationService', ReservationService);
 
-  ReservationService.$inject = ['$http', 'UserService'];
+    ReservationService.$inject = ['$http', 'UserService'];
 
-  /**
-   * [Instantiates a ReservationService singleton]
-   * @param  {Function} $http [AngularJS service that makes AJAX calls]
-   * @return {Object}         [returns ReservationService methods]
-   */
-  function ReservationService($http, UserService) {
     /**
-     * [returns all reservations from data API]
-     * @return {Promise}
+     * [Instantiates a ReservationService singleton]
+     * @param  {Function} $http [AngularJS service that makes AJAX calls]
+     * @return {Object}         [returns ReservationService methods]
      */
-    function getReservations() {
-      return $http({
-        url: 'https://penguin-hotelier-api.herokuapp.com/api/Reservations',
-        method: 'get',
-        headers: {
-          'Content-Type': 'application/JSON',
-          'Authorization': UserService.getToken()
+    function ReservationService($http, UserService) {
+        /**
+         * [returns all reservations from data API]
+         * @return {Promise}
+         */
+        function getReservations() {
+            return $http({
+                url: 'https://penguin-hotelier-api.herokuapp.com/api/Reservations',
+                method: 'get',
+                headers: {
+                    'Content-Type': 'application/JSON',
+                    'Authorization': UserService.getToken()
+              }
+            }).then(function handleResponse(response) {
+              return response.data;
+            });
         }
-      }).then(function handleResponse(response) {
-        return response.data;
-      });
-    }
 
-    function makeReservation(newRes) {
-      console.log("newRes object before json:", newRes);
-      let jsonObj = angular.toJson(newRes);
-      console.log('jsonObj is ', jsonObj);
-      console.log('token before $http:', UserService.getToken());
-      return $http({
-        url: 'https://penguin-hotelier-api.herokuapp.com/api/Reservations',
-        method: 'post',
-        headers: {
-          'Content-Type': 'application/JSON',
-          'Authorization': UserService.getToken()
-        },
-        data: jsonObj
-      }).then(function handleResponse(response) {
-        console.log('makeReservation response object is: ', response.data);
-        return response.data;
-      });
-    }
+        function makeReservation(newRes) {
+            let jsonObj = angular.toJson(newRes);
+            return $http({
+                url: 'https://penguin-hotelier-api.herokuapp.com/api/Reservations',
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/JSON',
+                    'Authorization': UserService.getToken()
+                },
+                data: jsonObj
+            }).then(function handleResponse(response) {
+                return response.data;
+            });
+        }
 
-    return {
-      getReservations: getReservations,
-      makeReservation: makeReservation
-    };
-  }
+        return {
+            getReservations: getReservations,
+            makeReservation: makeReservation
+        };
+    }
 }());

9bad56fd463c9b5134740c680cb21b9676d364b9
Brian Fiala <bgfiala@gmail.com>
2017-04-08 15:50:51 -0400

    fixed indentation

--- src/js/hotelier.module.js
+++ src/js/hotelier.module.js
@@ -1,68 +1,64 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular.module('hotelier', ['ui.router'])
+    angular.module('hotelier', ['ui.router'])
     .config(routerConfig)
     .run(setupAuthCheck);
 
-  routerConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+    routerConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
 
-  function routerConfig($stateProvider, $urlRouterProvider) {
+    function routerConfig($stateProvider, $urlRouterProvider) {
 
-    $urlRouterProvider.when('', '/');
+        $urlRouterProvider.when('', '/');
 
-    $urlRouterProvider.otherwise('/not-found');
+        $urlRouterProvider.otherwise('/not-found');
 
-    $stateProvider
-      .state({
-        name: 'create-guest',
-        url: '/create-guest',
-        templateUrl: 'views/create-guest.template.html',
-        controller: 'GuestController',
-        controllerAs: 'guestCtrl',
-        restricted: true
-      })
+        $stateProvider
+        .state({
+            name: 'create-guest',
+            url: '/create-guest',
+            templateUrl: 'views/create-guest.template.html',
+            controller: 'GuestController',
+            controllerAs: 'guestCtrl',
+            restricted: true
+        })
 
-      .state({
-        name: 'login',
-        url: '/login',
-        templateUrl: 'views/login.template.html',
-        controller: 'LoginController',
-        controllerAs: 'loginCtrl'
-      })
-      .state({
-        name: 'make-reservation',
-        url: '/make-reservation',
-        templateUrl: 'views/make-reservation.template.html',
-        controller: 'ReservationController',
-        controllerAs: 'resCtrl',
-        restricted: true
-      })
-      .state({
-        name: 'home',
-        url: '/',
-        templateUrl: 'views/home.template.html'
-      })
-      .state({
-          name: '404-not-found',
-          url: '/not-found',
-          templateUrl: 'views/not-found.template.html'
-      });
+        .state({
+            name: 'login',
+            url: '/login',
+            templateUrl: 'views/login.template.html',
+            controller: 'LoginController',
+            controllerAs: 'loginCtrl'
+        })
+        .state({
+            name: 'make-reservation',
+            url: '/make-reservation',
+            templateUrl: 'views/make-reservation.template.html',
+            controller: 'ReservationController',
+            controllerAs: 'resCtrl',
+            restricted: true
+        })
+        .state({
+            name: 'home',
+            url: '/',
+            templateUrl: 'views/home.template.html'
+        })
+        .state({
+            name: '404-not-found',
+            url: '/not-found',
+            templateUrl: 'views/not-found.template.html'
+        });
 
+        setupAuthCheck.$inject = ['$rootScope', '$state', 'UserService'];
+        function setupAuthCheck($rootScope, $state, UserService) {
 
-
-          setupAuthCheck.$inject = ['$rootScope', '$state', 'UserService'];
-          function setupAuthCheck($rootScope, $state, UserService) {
-
-              //   $on()  ==> addEventListener()
-              $rootScope.$on('$stateChangeStart', function checkLoginStatus(eventObj, toState) {
-                  if (toState.restricted && !UserService.getToken()) {
-                      eventObj.preventDefault();
-                      $state.go('login');
-                  }
-              });
-
-          }
-
-  }
+            //   $on()  ==> addEventListener()
+            $rootScope.$on('$stateChangeStart', function checkLoginStatus(eventObj, toState) {
+                if (toState.restricted && !UserService.getToken()) {
+                    eventObj.preventDefault();
+                    $state.go('login');
+                }
+            });
+        }
+    }
 }());

a66eee39d4cf8b93a529ee1223fb5ade6c954df3
Brian Fiala <bgfiala@gmail.com>
2017-04-08 15:29:29 -0400

    nav controller can handle logout and auth checking for logged in

--- src/js/navigation.controller.js
+++ src/js/navigation.controller.js
@@ -1,6 +1,27 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  
+    angular.module('hotelier')
+    .controller('NavController', NavController);
 
+    NavController.$inject = ['UserService'];
+    function NavController(UserService) {
+        let nc = this;
+
+        /**
+        * Allows the user to logout
+        * @return {Void}
+        */
+        nc.logout = function logout() {
+            UserService.logout();
+        };
+
+        /**
+        * Determines if a user is presently logged in.
+        * @return {Boolean} True if logged in.
+        */
+        vm.loggedIn = function loggedIn() {
+            return !!UserService.isLoggedIn();
+        };
+    }
 }());

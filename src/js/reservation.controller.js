823635d716d4fcb3bd093cd1f8878829f988e3b8
David Steed <davidasteed@gmail.com>
2017-04-07 21:14:13 -0400

    initial commit - discovered that I need to stop work on this branch until I have functions available to obtain Room object and Guest objects that contain data needed to population the 'view reservation page'

--- src/js/reservation.controller.js
+++ src/js/reservation.controller.js
@@ -26,15 +26,32 @@
          * @return {void}
          */
         vm.makeReservation = function makeReservation(newRes) {
-          // basic validation of form Object
-          if (!newRes || newRes.length === 0 ||
-            Array.isArray(newRes) || typeof(newRes) !== 'object') {
-              vm.hasError = true;
-              vm.errorMessage = 'Sorry, invalid reservation information provided';
-              return;
+            // basic validation of form Object
+            if (!newRes || newRes.length === 0 ||
+                Array.isArray(newRes) || typeof(newRes) !== 'object') {
+                vm.hasError = true;
+                vm.errorMessage = 'Sorry, invalid reservation information provided';
+                return;
             }
 
-        ReservationService.makeReservation(newRes);
+          ReservationService.makeReservation(newRes)
+              .then(function handleResData(resData){
+                  vm.storedReservation = resData;
+                  console.log('resData is', resData);
+              })
+              .catch(function handleErrors(errResponse) {
+                  console.warn(errResponse);
+                  vm.hasError = true;
+                  if (errResponse.status === 401) {
+                      vm.errorMessage = 'We need to log in first';
+                  } else if (errResponse.status === 404) {
+                      vm.errorMessage = 'URL fails to resolve to the API site';
+                  } else if (errResponse.status === 422) {
+                      vm.errorMessage = 'Unexpected reservation data provided';
+                  } else {
+                      vm.errorMessage = 'Server error!';
+                  }
+              });
       };
 
     }

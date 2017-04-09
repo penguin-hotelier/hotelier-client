96784dcfa7b84d03806376bc65b516ac5733bc3f
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-04-08 21:50:03 -0400

    For testing the user service, set up a mockUserService that is created before each test was run which includes the creating a foe token

--- src/test/specs/user.service.spec.js
+++ src/test/specs/user.service.spec.js
@@ -5,11 +5,20 @@
 
     describe('login service', function() {
         let UserService;
+        let mockUserService = {};
 
         beforeEach(module('hotelier'));
 
+        beforeEach(module(function($provide) {
+            $provide.value('UserService', UserService);
+        }));
+
         beforeEach(inject(function(_UserService_) {
             UserService = _UserService_;
+
+            mockUserService.getToken = function getToken() {
+                return '7y3h68j940k30mr7j4rr3w96h37g60';
+            };
         }));
 
         describe('login', function() {
@@ -23,10 +32,11 @@
                     done('We should not resolve with a bad argument');
                 })
                 .catch(function handleError(err) {
+                    //TODO add assertions on the err- see shopular notes
                     done();
                 });
             });
         });
-        
+
     });
 }());
